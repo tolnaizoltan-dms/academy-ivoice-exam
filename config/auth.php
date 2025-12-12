fc47aa8efc260b4b06c911eea7bde9eb6946cc62
Tolnai Zoltan <tolnai.zoltan@dmsone.hu>
2025-12-12 13:34:38 +0100

    Update application name and comment out user provider configuration in auth settings

--- config/auth.php
+++ config/auth.php
@@ -60,10 +60,10 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
+        // 'users' => [
+        //     'driver' => 'eloquent',
+        //     'model' => env('AUTH_MODEL', App\Models\User::class),
+        // ],
 
         // 'users' => [
         //     'driver' => 'database',
